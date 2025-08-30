8fac9f136643c3d56afe1921e6c35205f8980999
tech-builders <atul@techbuilders.in>
2025-08-30 12:57:18 +0530

    Intialize all block in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,66 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:task_manager/core/theme/app_theme.dart';
+import 'package:task_manager/features/task_manager/data/datasources/local_data_source.dart';
+import 'package:task_manager/features/task_manager/data/repositories/task_repository_impl.dart';
+import 'package:task_manager/features/task_manager/domain/usecases/add_task.dart';
+import 'package:task_manager/features/task_manager/domain/usecases/filter_tasks.dart';
+import 'package:task_manager/features/task_manager/domain/usecases/get_tasks.dart';
+import 'package:task_manager/features/task_manager/domain/usecases/reorder_tasks.dart';
+import 'package:task_manager/features/task_manager/domain/usecases/update_task.dart';
+import 'package:task_manager/features/task_manager/presentation/blocs/task_bloc/task_bloc.dart';
+import 'package:task_manager/features/task_manager/presentation/blocs/task_bloc/task_event.dart';
+import 'package:task_manager/features/task_manager/presentation/blocs/theme_cubit.dart';
+import 'package:task_manager/features/task_manager/presentation/screens/task_list_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  final local = SqfliteLocalDataSource();
+  final repo = TaskRepositoryImpl(local);
+
+  runApp(MyApp(repo: repo));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+  const MyApp({super.key, required this.repo});
+  final TaskRepositoryImpl repo;
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => ThemeCubit()),
+        BlocProvider(
+          create: (_) => TaskBloc(
+            getTasks: GetTasks(repo),
+            addTask: AddTask(repo),
+            updateTask: UpdateTask(repo),
+            filterTasks: FilterTasks(repo),
+            reorderTasks: ReorderTasks(repo),
+          )..add(TaskStarted()),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      ],
+      child: BlocBuilder<ThemeCubit, bool>(
+        builder: (context, isDarkMode) {
+          return MaterialApp(
+            title: 'Task Manager demo',
+            themeMode: isDarkMode ? ThemeMode.dark : ThemeMode.light,
+            theme: AppTheme.lightTheme,
+            darkTheme: AppTheme.darkTheme,
+            home: const TaskListScreen(),
+          );
+        },
       ),
     );
   }
